33693f21a45e35791e4fa86cea284243377d96f1
Alex <opetrovskyi423@gmail.com>
2021-06-12 13:21:31 +0300

    HowWeWork_update

--- src/components/HowWeWork/HowWeWork.jsx
+++ src/components/HowWeWork/HowWeWork.jsx
@@ -12,54 +12,60 @@ const HowWeWork = () => {
       <Container>
         <h2 className={style.mainWorkTitle}>Як ми працюємо?</h2>
         
-        <div className={style.allWorkContainer} >
-        <div className={style.workSubContainer}>
-          <div className={style.workSubIcon}>
-            <IoPhonePortrait />
-            <div className={style.workSubtitle}>
-              TITLE
+        <div className={style.allWorkContainer}>
+          <div className={style.workSubContainer}>
+            <div className={style.workSubIcon}>
+              <IoPhonePortrait/>
+              <h2 className={style.workSubtitle}>
+                Дзвінок в майстерню
+              </h2>
               <div className={style.workSubText}>
-                SomeText
+                Ви залишаєте заявку на зворотний дзвінок або телефонуйте в нашу майстерню по телефону, вказаному на
+                сайті.
+              </div>
+            
+            </div>
+          </div>
+          
+          <div className={style.workSubContainer}>
+            <div className={style.workSubIcon}>
+              <GrUserManager/>
+              <h2 className={style.workSubtitle}>
+                Консультація
+              </h2>
+              <div className={style.workSubText}>
+                Після консультації наш майстер записує Вас на зручний для ремонту день і називає попередню вартість
+                робіт.
+              
               </div>
             </div>
           </div>
-        </div>
-  
-        <div className={style.workSubContainer}>
-          <div className={style.workSubIcon}>
-            <GrUserManager />
-            <div className={style.workSubtitle}>
-              TITLE
+          
+          <div className={style.workSubContainer}>
+            <div className={style.workSubIcon}>
+              <GiMagnifyingGlass/>
+              <h2 className={style.workSubtitle}>
+                Відвідування майстерні
+              </h2>
               <div className={style.workSubText}>
-                SomeText
+                У майстерні наш фахівець ретельно оглядає пошкодження автомобіля для складання точної вартості ремонту.
               </div>
             </div>
           </div>
-        </div>
-  
-        <div className={style.workSubContainer}>
-          <div className={style.workSubIcon}>
-          <GiMagnifyingGlass />
-            <div className={style.workSubtitle}>
-              TITLE
+          
+          <div className={style.workSubContainer}>
+            <div className={style.workSubIcon}>
+              <FaCar/>
+              <h2 className={style.workSubtitle}>
+                Ремонт без фарбування
+              </h2>
               <div className={style.workSubText}>
-                SomeText
+                Після узгодження ціни і термінів виконання наші майстри негайно приступають до ремонту Вашого
+                автомобіля.
+              
               </div>
             </div>
           </div>
-        </div>
-  
-        <div className={style.workSubContainer}>
-          <div className={style.workSubIcon}>
-            <FaCar />
-            <div className={style.workSubtitle}>
-              TITLE
-              <div className={style.workSubText}>
-                SomeText
-              </div>
-            </div>
-          </div>
-        </div>
         </div>
       </Container>
     </div>
