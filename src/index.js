438c248551edaf5411e560e4d65476a4ff4e897a
Alex <opetrovskyi423@gmail.com>
2023-04-14 17:41:17 +0200

    fix base url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/PDR_Zbarazh_React">
             <App />
         </BrowserRouter>
     </StrictMode>
